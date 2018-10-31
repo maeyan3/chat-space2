80a838db4e4092de75541d0d6e629fac8815dc70
T:MASTER <ssstechz@TMASTERnoMacBook-puro.local>
2018-11-01 00:56:53 +0900

    .HTMLを追加した分、メッセージ画面を下にスクロールする

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -53,6 +53,7 @@ $('.js-form').on('submit', function(){
      .done(function(data){
        var html = buildHTML(data);
        $('.messages').append(html);
+       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
        $('form')[0].reset();
      })
 });
